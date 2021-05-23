5be1bf32e3c93e01e496983484f372e2e4fcecfc
Irwin Villalobos <irwin.villalobos@cimat.mx>
2021-05-23 13:56:07 -0500

    Mapeo Exponencial: Se agrego la opcion de cambiar el tipo de mapeo de color entre lineal y exponencial

--- draw.js
+++ draw.js
@@ -6,7 +6,9 @@ var MandelLienzo = MandelCanvas.getContext("2d");
 var Wdth = MandelCanvas.width;
 var Hgth = MandelCanvas.height;
 
-document.getElementById("drawButton").addEventListener("click", DrawMandelSet);
+document.getElementById("drawButton").addEventListener("click", drawMandelSet);
+document.getElementById("mapTypeBoxLineal").addEventListener("change", changeBoxesDifferentToLineal);
+document.getElementById("mapTypeBoxExponential").addEventListener("change", changeBoxesDifferentToExponential);
 
 function paintPixel(x, y, color){
 	MandelLienzo.fillStyle = color;
@@ -22,7 +24,7 @@ function MapToComplexPoint(x, y){
 	return math.complex(zoomH*(y/Hgth - 0.5)+centerRe, zoomH*(x/Hgth - 0.5)+centerIm);	
 }
 
-var maxIter;
+var maxIter = 12;
 
 function iterationsToGetOutOfMandelSet(C){
 
@@ -42,15 +44,33 @@ function iterationsToGetOutOfMandelSet(C){
 }
 
 var closerDarker = false;
+var mapType = 1;
+var lambda = 0.1;
+// mapType = 0 -> Lineal
+// mapType = 1 -> Exponencial
 
-function MapIterationsToPrimaryCorlor(x, c){
-	if(closerDarker)
-		return parseInt(((c-255)*x)/(maxIter-2) + (255*(maxIter-1)-c)/(maxIter-2));
-	else
-		return parseInt(((255-c)*x)/(maxIter-2) + (c*(maxIter-1)-255)/(maxIter-2));
+function mapIterationsToPrimaryColor(x, c){
+	if(mapType == 0){
+		if(closerDarker)
+			return parseInt(((c-255)*x)/(maxIter-2) + (255*(maxIter-1)-c)/(maxIter-2));
+		else
+			return parseInt(((255-c)*x)/(maxIter-2) + (c*(maxIter-1)-255)/(maxIter-2));
+	}
+	else if(mapType == 1){
+		if(!closerDarker){
+			var a = (255 - c)/(Math.exp(-lambda*maxIter) - 1.0);
+			var b = c - a;
+			return parseInt(a*Math.exp(-lambda*x)+b);
+		}
+		else{
+			var a = (c - 255)/(Math.exp(-lambda*maxIter) - 1.0);
+			var b = 255 - a;
+			return parseInt(a*Math.exp(-lambda*x)+b);
+		}
+	}
 }
 
-function DrawMandelSet(){
+function drawMandelSet(){
 
 	maxIter = parseInt(document.getElementById("iteracionesBox").value);
 	var rBackBase = parseInt(document.getElementById("redBack").value);
@@ -60,6 +80,12 @@ function DrawMandelSet(){
 	var gSet = parseInt(document.getElementById("greenSet").value);
 	var bSet = parseInt(document.getElementById("blueSet").value);
 	closerDarker = document.getElementById("closerDarkerCheckBox").checked;
+	if(document.getElementById("mapTypeBoxLineal").checked){
+		mapType = 0;
+	}
+	else if(document.getElementById("mapTypeBoxExponential").checked){
+		mapType = 1;
+	}
 
 	var SetColor = "rgb(" + rSet + "," + gSet + "," + bSet + ")";
 
@@ -70,12 +96,26 @@ function DrawMandelSet(){
 			if(iterations == maxIter)
 				paintPixel(w, h, SetColor);
 			else{
-				var rPix = MapIterationsToPrimaryCorlor(iterations, rBackBase);
-				var gPix = MapIterationsToPrimaryCorlor(iterations, gBackBase);
-				var bPix = MapIterationsToPrimaryCorlor(iterations, bBackBase);				
+				var rPix = mapIterationsToPrimaryColor(iterations, rBackBase);
+				var gPix = mapIterationsToPrimaryColor(iterations, gBackBase);
+				var bPix = mapIterationsToPrimaryColor(iterations, bBackBase);				
 				var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
 				paintPixel(w, h, pixColor);
 			}
 		}
 	}
+}
+
+function changeBoxesDifferentToLineal(){
+	if(document.getElementById("mapTypeBoxLineal").checked == 1)
+		document.getElementById("mapTypeBoxExponential").checked = 0;
+	else
+		document.getElementById("mapTypeBoxExponential").checked = 1;
+}
+
+function changeBoxesDifferentToExponential(){
+	if(document.getElementById("mapTypeBoxExponential").checked == 1)
+		document.getElementById("mapTypeBoxLineal").checked = 0;
+	else
+		document.getElementById("mapTypeBoxLineal").checked = 1;
 }
