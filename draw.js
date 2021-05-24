9b65c013c7303dd21e36752347d9128ab1d41d31
Irwin Villalobos <irwin.villalobos@cimat.mx>
2021-05-23 20:52:42 -0500

    Cuatro puntos por pixel: Se agrego la plantilla y Macros para implementar distintos metodos para pintar cada pixel

--- draw.js
+++ draw.js
@@ -20,8 +20,8 @@ var centerIm = 0;
 var zoomH = 2.4;
 var ColNums = 10;
 
-function MapToComplexPoint(x, y){
-	return math.complex(zoomH*(y/Hgth - 0.5)+centerRe, zoomH*(x/Hgth - 0.5)+centerIm);	
+function MapToComplexPoint(h, w){
+	return math.complex(zoomH*(w/Hgth - 0.5)+centerRe, zoomH*(h/Hgth - 0.5)+centerIm);	
 }
 
 var maxIter = 12;
@@ -46,17 +46,17 @@ function iterationsToGetOutOfMandelSet(C){
 var closerDarker = false;
 var mapType = 1;
 var lambda = 0.1;
-// mapType = 0 -> Lineal
-// mapType = 1 -> Exponencial
+var MAP_TYPE_LINEAL = 0;      // -> Lineal
+var MAP_TYPE_EXPONENTIAL = 1; // -> Exponencial
 
 function mapIterationsToPrimaryColor(x, c){
-	if(mapType == 0){
+	if(mapType == MAP_TYPE_LINEAL){
 		if(closerDarker)
 			return parseInt(((c-255)*x)/(maxIter-2) + (255*(maxIter-1)-c)/(maxIter-2));
 		else
 			return parseInt(((255-c)*x)/(maxIter-2) + (c*(maxIter-1)-255)/(maxIter-2));
 	}
-	else if(mapType == 1){
+	else if(mapType == MAP_TYPE_EXPONENTIAL){
 		if(!closerDarker){
 			var a = (255 - c)/(Math.exp(-lambda*maxIter) - 1.0);
 			var b = c - a;
@@ -70,16 +70,59 @@ function mapIterationsToPrimaryColor(x, c){
 	}
 }
 
+var rBackBase, gBackBase, bBackBase;
+var SetColor;
+
+var paintPixelMethod = 0;
+
+var PAINT_PIXEL_METHOD_1TO1 = 0;             // -> Un punto por pixel.
+var PAINT_PIXEL_METHOD_4TO1_INDV_CROSS = 1;  // -> Cuatro puntos por pixel. No comparten puntos. Arreglo en cruz.
+var PAINT_PIXEL_METHOD_4TO1_INDV_PLUS = 2;   // -> Cuatro puntos por pixel. No comparten puntos. Arreglo en mas.
+var PAINT_PIXEL_METHOD_4TO1_SHRD_CROSS = 3;  // -> Cuatro puntos por pixel. Comparten puntos. Arreglo en cruz.
+var PAINT_PIXEL_METHOD_4TO1_SHRD_PLUS = 4;   // -> Cuatro puntos por pixel. Comparten puntos. Arreglo en mas.
+
+function assignPixelColor(w, h){
+	if(paintPixelMethod == PAINT_PIXEL_METHOD_1TO1){
+		var point = MapToComplexPoint(h, w);
+		var iterations = iterationsToGetOutOfMandelSet(point);
+		if(iterations == maxIter)
+			paintPixel(w, h, SetColor);
+		else{
+			var rPix = mapIterationsToPrimaryColor(iterations, rBackBase);
+			var gPix = mapIterationsToPrimaryColor(iterations, gBackBase);
+			var bPix = mapIterationsToPrimaryColor(iterations, bBackBase);				
+			var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
+			paintPixel(w, h, pixColor);
+		}
+	}
+	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_INDV_CROSS){
+
+	}
+	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_INDV_PLUS){
+
+	}
+	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_SHRD_CROSS){
+
+	}
+	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_SHRD_PLUS){
+
+	}
+}
+
 function drawMandelSet(){
 
 	maxIter = parseInt(document.getElementById("iteracionesBox").value);
-	var rBackBase = parseInt(document.getElementById("redBack").value);
-	var gBackBase = parseInt(document.getElementById("greenBack").value);
-	var bBackBase = parseInt(document.getElementById("blueBack").value);
+	
+	rBackBase = parseInt(document.getElementById("redBack").value);
+	gBackBase = parseInt(document.getElementById("greenBack").value);
+	bBackBase = parseInt(document.getElementById("blueBack").value);
+	
 	var rSet = parseInt(document.getElementById("redSet").value);
 	var gSet = parseInt(document.getElementById("greenSet").value);
 	var bSet = parseInt(document.getElementById("blueSet").value);
+	
 	closerDarker = document.getElementById("closerDarkerCheckBox").checked;
+	
 	if(document.getElementById("mapTypeBoxLineal").checked){
 		mapType = 0;
 	}
@@ -87,21 +130,11 @@ function drawMandelSet(){
 		mapType = 1;
 	}
 
-	var SetColor = "rgb(" + rSet + "," + gSet + "," + bSet + ")";
+	SetColor = "rgb(" + rSet + "," + gSet + "," + bSet + ")";
 
 	for(var w=0; w<Wdth; w++){
 		for(var h=0; h<Hgth; h++){
-			var point = MapToComplexPoint(h, w);
-			var iterations = iterationsToGetOutOfMandelSet(point);
-			if(iterations == maxIter)
-				paintPixel(w, h, SetColor);
-			else{
-				var rPix = mapIterationsToPrimaryColor(iterations, rBackBase);
-				var gPix = mapIterationsToPrimaryColor(iterations, gBackBase);
-				var bPix = mapIterationsToPrimaryColor(iterations, bBackBase);				
-				var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-				paintPixel(w, h, pixColor);
-			}
+			assignPixelColor(w, h); 
 		}
 	}
 }
