4be6e5ffa5aeee0dfe1e90f2bb95d50dec121689
Irwin Villalobos <irwin.villalobos@cimat.mx>
2021-05-29 02:13:18 -0500

    Cuatro puntos por pixel: Se juntaron los cinco metodos diferentes de pintar pixeles para ahorrar codigo y se agrego la modalidad para cambiar esta configuracion en el HTML

--- draw.js
+++ draw.js
@@ -1,3 +1,4 @@
+// Código en javascript
 
 console.log("La vida es dura, pero es mas dura la verdura.");
 var MandelCanvas = document.getElementById("MandelPicture");
@@ -9,6 +10,12 @@ var Hgth = MandelCanvas.height;
 document.getElementById("drawButton").addEventListener("click", drawMandelSet);
 document.getElementById("mapTypeBoxLineal").addEventListener("change", changeBoxesDifferentToLineal);
 document.getElementById("mapTypeBoxExponential").addEventListener("change", changeBoxesDifferentToExponential);
+document.getElementById("1to1Box").addEventListener("change", changeBoxesDifferentTo1to1);
+document.getElementById("4to1Box").addEventListener("change", changeBoxesDifferentTo4to1);
+document.getElementById("crossBox").addEventListener("change", changeBoxesDifferentToCross);
+document.getElementById("plusBox").addEventListener("change", changeBoxesDifferentToPlus);
+document.getElementById("pixelsDoShareBox").addEventListener("change", changeBoxesDifferentToDoShare);
+document.getElementById("pixelsDontShareBox").addEventListener("change", changeBoxesDifferentToDontShare);
 
 function paintPixel(x, y, color){
 	MandelLienzo.fillStyle = color;
@@ -76,44 +83,42 @@ var SetColor;
 
 var pointsPerPixel = 4;
 var pointsConfiguration = 0; 
-var pointsShareInformation = true;
+var pixelsShareInformation = true;
 
 var POINTS_CONFIGURATION_CROSS = 0; // -> Configuración en tacha.
 var POINTS_CONFIGURATION_PLUS = 1;  // -> Configuración en cruz.
-
-/*
-var PAINT_PIXEL_METHOD_1TO1 = 0;             // -> Un punto por pixel.
-var PAINT_PIXEL_METHOD_4TO1_INDV_CROSS = 1;  // -> Cuatro puntos por pixel. No comparten puntos. Arreglo en cruz.
-var PAINT_PIXEL_METHOD_4TO1_INDV_PLUS = 2;   // -> Cuatro puntos por pixel. No comparten puntos. Arreglo en mas.
-var PAINT_PIXEL_METHOD_4TO1_SHRD_CROSS = 3;  // -> Cuatro puntos por pixel. Comparten puntos. Arreglo en cruz.
-var PAINT_PIXEL_METHOD_4TO1_SHRD_PLUS = 4;   // -> Cuatro puntos por pixel. Comparten puntos. Arreglo en mas.
-*/
 
 var crossStep       = [ [1, 1],  [1, -1],  [-1,  1],  [-1, -1] ];
 var plusStep        = [ [0, 1],  [1,  0],  [0,  -1],  [-1,  0] ];
 var crossMatrixStep = [ [0, 0],  [0,  1],  [1,   0],  [1,   1] ];
 var plusMatrixStep  = [ [0, 0],  [0,  1],  [0,   2],  [1,   1] ];
 
+var delta = 1;
 var iterMatrix = new Array(Hgth+1);
-
-for(var i=0; i<=Hgth; i++){
-	iterMatrix[i] = new Array(2*Wdth+2);
-	for(var j=0; j<=2*Wdth+1; j++){
-		iterMatrix[i][j] = -1;
-	}
-}
 
 function assignPixelColor(w, h){
 	var rPix = 0;
 	var gPix = 0;
 	var bPix = 0;
-	var delta = 0.5*(zoomH/Hgth);
-	for(var i=0; i<4; i++){
-		var iterations = iterMatrix[h+crossMatrixStep[i][0]][w+crossMatrixStep[i][1]];
+	
+	for(var i=0; i<pointsPerPixel; i++){
+		
+		var iterations = -1;
+		if(pixelsShareInformation){
+			if(pointsConfiguration == POINTS_CONFIGURATION_CROSS)
+				iterations = iterMatrix[h+crossMatrixStep[i][0]][w+crossMatrixStep[i][1]];
+			else if(pointsConfiguration == POINTS_CONFIGURATION_PLUS)
+				iterations = iterMatrix[h+plusMatrixStep[i][0]][w+plusMatrixStep[i][1]];
+		}
 		if(iterations == -1){
-			var point = MapToComplexPoint(h+delta*crossStep[i][0], w+delta*crossStep[i][1]);
+			var point;
+			if(pointsConfiguration == POINTS_CONFIGURATION_CROSS)
+				point = MapToComplexPoint(h+delta*crossStep[i][0], w+delta*crossStep[i][1]);
+			else
+				point = MapToComplexPoint(h+delta*plusStep[i][0], w+delta*plusStep[i][1]);				
 			iterations = iterationsToGetOutOfMandelSet(point);
 		}
+
 		if(iterations == maxIter){
 			rPix += rSet;
 			gPix += gSet;
@@ -125,146 +130,11 @@ function assignPixelColor(w, h){
 			bPix += mapIterationsToPrimaryColor(iterations, bBackBase);				
 		}
 	}
-	rPix /= 4.0; rPix = parseInt(rPix);
-	gPix /= 4.0; gPix = parseInt(gPix);
-	bPix /= 4.0; bPix = parseInt(bPix);
+	rPix /= pointsPerPixel; rPix = parseInt(rPix);
+	gPix /= pointsPerPixel; gPix = parseInt(gPix);
+	bPix /= pointsPerPixel; bPix = parseInt(bPix);
 	var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
 	paintPixel(w, h, pixColor);
-}
-
-function assignPixelColor(w, h){
-	if(paintPixelMethod == PAINT_PIXEL_METHOD_1TO1){
-		var point = MapToComplexPoint(h, w);
-		var iterations = iterationsToGetOutOfMandelSet(point);
-		SetColor = "rgb(" + rSet + "," + gSet + "," + bSet + ")";
-		if(iterations == maxIter)
-			paintPixel(w, h, SetColor);
-		else{
-			var rPix = mapIterationsToPrimaryColor(iterations, rBackBase);
-			var gPix = mapIterationsToPrimaryColor(iterations, gBackBase);
-			var bPix = mapIterationsToPrimaryColor(iterations, bBackBase);				
-			var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-			paintPixel(w, h, pixColor);
-		}
-	}
-	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_INDV_CROSS){
-		if(h == 0 && w == 0){
-			console.log("Usando 4 a 1 Indv Cross");
-		}
-		var rPix = 0;
-		var gPix = 0;
-		var bPix = 0;
-		var delta = 0.25*(zoomH/Hgth);
-		for(var i=0; i<4; i++){
-			var point = MapToComplexPoint(h+delta*crossStep[i][0], w+delta*crossStep[i][1]);
-			var iterations = iterationsToGetOutOfMandelSet(point);
-			if(iterations == maxIter){
-				rPix += rSet;
-				gPix += gSet;
-				bPix += bSet;
-			}
-			else{
-				rPix += mapIterationsToPrimaryColor(iterations, rBackBase);
-				gPix += mapIterationsToPrimaryColor(iterations, gBackBase);
-				bPix += mapIterationsToPrimaryColor(iterations, bBackBase);				
-			}
-		}
-		rPix /= 4.0; rPix = parseInt(rPix);
-		gPix /= 4.0; gPix = parseInt(gPix);
-		bPix /= 4.0; bPix = parseInt(bPix);
-		var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-		paintPixel(w, h, pixColor);
-	}
-	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_INDV_PLUS){
-		if(h == 0 && w == 0){
-			console.log("Usando 4 a 1 Indv Plus");
-		}
-		var rPix = 0;
-		var gPix = 0;
-		var bPix = 0;
-		var delta = 0.25*(zoomH/Hgth);
-		for(var i=0; i<4; i++){
-			var point = MapToComplexPoint(h+delta*plusStep[i][0], w+delta*plusStep[i][1]);
-			var iterations = iterationsToGetOutOfMandelSet(point);
-			if(iterations == maxIter){
-				rPix += rSet;
-				gPix += gSet;
-				bPix += bSet;
-			}
-			else{
-				rPix += mapIterationsToPrimaryColor(iterations, rBackBase);
-				gPix += mapIterationsToPrimaryColor(iterations, gBackBase);
-				bPix += mapIterationsToPrimaryColor(iterations, bBackBase);				
-			}
-		}
-		rPix /= 4.0; rPix = parseInt(rPix);
-		gPix /= 4.0; gPix = parseInt(gPix);
-		bPix /= 4.0; bPix = parseInt(bPix);
-		var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-		paintPixel(w, h, pixColor);
-	}
-	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_SHRD_CROSS){
-		if(h == 0 && w == 0){
-			console.log("Usando 4 a 1 Shrd Cross");
-		}
-		var rPix = 0;
-		var gPix = 0;
-		var bPix = 0;
-		var delta = 0.5*(zoomH/Hgth);
-		for(var i=0; i<4; i++){
-			var iterations = iterMatrix[h+crossMatrixStep[i][0]][w+crossMatrixStep[i][1]];
-			if(iterations == -1){
-				var point = MapToComplexPoint(h+delta*crossStep[i][0], w+delta*crossStep[i][1]);
-				iterations = iterationsToGetOutOfMandelSet(point);
-			}
-			if(iterations == maxIter){
-				rPix += rSet;
-				gPix += gSet;
-				bPix += bSet;
-			}
-			else{
-				rPix += mapIterationsToPrimaryColor(iterations, rBackBase);
-				gPix += mapIterationsToPrimaryColor(iterations, gBackBase);
-				bPix += mapIterationsToPrimaryColor(iterations, bBackBase);				
-			}
-		}
-		rPix /= 4.0; rPix = parseInt(rPix);
-		gPix /= 4.0; gPix = parseInt(gPix);
-		bPix /= 4.0; bPix = parseInt(bPix);
-		var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-		paintPixel(w, h, pixColor);
-	}
-	else if(paintPixelMethod == PAINT_PIXEL_METHOD_4TO1_SHRD_PLUS){
-		if(h == 0 && w == 0){
-			console.log("Usando 4 a 1 Shrd Cross");
-		}
-		var rPix = 0;
-		var gPix = 0;
-		var bPix = 0;
-		var delta = 0.5*(zoomH/Hgth);
-		for(var i=0; i<4; i++){
-			var iterations = iterMatrix[h+plusMatrixStep[i][0]][2*w+plusMatrixStep[i][1]];
-			if(iterations == -1){
-				var point = MapToComplexPoint(h+delta*plusStep[i][0], w+delta*plusStep[i][1]);
-				iterations = iterationsToGetOutOfMandelSet(point);
-			}
-			if(iterations == maxIter){
-				rPix += rSet;
-				gPix += gSet;
-				bPix += bSet;
-			}
-			else{
-				rPix += mapIterationsToPrimaryColor(iterations, rBackBase);
-				gPix += mapIterationsToPrimaryColor(iterations, gBackBase);
-				bPix += mapIterationsToPrimaryColor(iterations, bBackBase);				
-			}
-		}
-		rPix /= 4.0; rPix = parseInt(rPix);
-		gPix /= 4.0; gPix = parseInt(gPix);
-		bPix /= 4.0; bPix = parseInt(bPix);
-		var pixColor = "rgb(" + rPix + "," + gPix + "," + bPix + ")";
-		paintPixel(w, h, pixColor);
-	}
 }
 
 function drawMandelSet(){
@@ -281,26 +151,54 @@ function drawMandelSet(){
 	
 	closerDarker = document.getElementById("closerDarkerCheckBox").checked;
 	
-	if(document.getElementById("mapTypeBoxLineal").checked){
+	if(document.getElementById("mapTypeBoxLineal").checked)
 		mapType = 0;
-	}
-	else if(document.getElementById("mapTypeBoxExponential").checked){
+	else if(document.getElementById("mapTypeBoxExponential").checked)
 		mapType = 1;
+
+	if(document.getElementById("pixelsDoShareBox").checked)
+		pixelsShareInformation = true;
+	else if(document.getElementById("pixelsDontShareBox").checked)
+		pixelsShareInformation = false;
+
+	if(document.getElementById("1to1Box").checked)
+		pointsPerPixel = 1;
+	else if(document.getElementById("4to1Box").checked)
+		pointsPerPixel = 4;
+
+	if(document.getElementById("crossBox").checked)
+		pointsConfiguration = POINTS_CONFIGURATION_CROSS;
+	else if(document.getElementById("plusBox").checked)
+		pointsConfiguration = POINTS_CONFIGURATION_PLUS;
+
+	if(pointsPerPixel == 1)
+		delta = 0;
+	else if(pointsPerPixel == 4){
+		if(pixelsShareInformation)
+			delta = 0.5*(zoomH/Hgth);
+		else
+			delta = 0.25*(zoomH/Hgth);
 	}
 
-	
+	if(pixelsShareInformation){
+		if(pointsConfiguration == POINTS_CONFIGURATION_CROSS){
+			for(var i=0; i<=Hgth; i++){
+				iterMatrix[i] = new Array(Wdth+1);
+				for(var j=0; j<=Wdth; j++)
+					iterMatrix[i][j] = -1;
+			}
+		}
+		else if(pointsConfiguration == POINTS_CONFIGURATION_PLUS){
+			for(var i=0; i<=Hgth; i++){
+				iterMatrix[i] = new Array(2*Wdth+2);
+				for(var j=0; j<=2*Wdth+1; j++)
+					iterMatrix[i][j] = -1;
+			}
+		}	
+	}
 
 	for(var w=0; w<Wdth; w++){
 		for(var h=0; h<Hgth; h++){
-			switch(paintPixelMethod){
-				case PAINT_PIXEL_METHOD_1TO1:
-
-				case PAINT_PIXEL_METHOD_4TO1_INDV_CROSS:
-
-				case PAINT_PIXEL_METHOD_4TO1_INDV_PLUS:
-
-				case PA
-			}
 			assignPixelColor(w, h); 
 		}
 	}
@@ -320,4 +218,46 @@ function changeBoxesDifferentToExponential(){
 		document.getElementById("mapTypeBoxLineal").checked = 0;
 	else
 		document.getElementById("mapTypeBoxLineal").checked = 1;
+}
+
+function changeBoxesDifferentTo1to1(){
+	if(document.getElementById("1to1Box").checked == 1)
+		document.getElementById("4to1Box").checked = 0;
+	else
+		document.getElementById("4to1Box").checked = 1;
+}
+
+function changeBoxesDifferentTo4to1(){
+	if(document.getElementById("4to1Box").checked == 1)
+		document.getElementById("1to1Box").checked = 0;
+	else
+		document.getElementById("1to1Box").checked = 1;
+}
+
+function changeBoxesDifferentToCross(){
+	if(document.getElementById("crossBox").checked == 1)
+		document.getElementById("plusBox").checked = 0;
+	else
+		document.getElementById("plusBox").checked = 1;
+}
+
+function changeBoxesDifferentToPlus(){
+	if(document.getElementById("plusBox").checked == 1)
+		document.getElementById("crossBox").checked = 0;
+	else
+		document.getElementById("crossBox").checked = 1;
+}
+
+function changeBoxesDifferentToDoShare(){
+	if(document.getElementById("pixelsDoShareBox").checked == 1)
+		document.getElementById("pixelsDontShareBox").checked = 0;
+	else
+		document.getElementById("pixelsDontShareBox").checked = 1;
+}
+
+function changeBoxesDifferentToDontShare(){
+	if(document.getElementById("pixelsDontShareBox").checked == 1)
+		document.getElementById("pixelsDoShareBox").checked = 0;
+	else
+		document.getElementById("pixelsDoShareBox").checked = 1;
 }
